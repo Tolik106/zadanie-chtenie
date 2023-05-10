0595677366d95255ae91e329220ef3dbfc87c1e8
Анатолий Савин <beningtonchess@yandex.ru>
2023-05-10 20:28:30 +0300

    zadanie2

--- zadanie.py
+++ zadanie.py
@@ -4,7 +4,7 @@ from pprint import pprint
 with open('file.txt', 'rt', encoding='utf-8') as file:
     cook_book = {}
     for line in file:
-        dish = line.strip()
+        dishnes = line.strip()
         quantities = int(file.readline())
         ingredients = []
         for ingridient in range(quantities):
@@ -20,6 +20,33 @@ with open('file.txt', 'rt', encoding='utf-8') as file:
             ingredients.append(book)
 
         file.readline()
-        cook_book[dish] = ingredients
+        cook_book[dishnes] = ingredients
 
     pprint(cook_book, sort_dicts=False)
+
+print()
+print()
+def get_shop_list_by_dishes(dishes, person_count):
+    result = {}
+    for dish in dishes:
+        recipe = cook_book[dish]
+        for ingridient in recipe:
+            key = ingridient['ingredient_name']
+            if key not in result:
+                new_dict = {
+                    'measure' : ingridient['measure'],
+                    'quantity' : int(ingridient['quantity']) * person_count
+
+                }
+                result[key] = new_dict
+            else:
+                result[key]['quantity'] += int(ingridient['quantity']) * person_count
+
+    pprint(result, sort_dicts=False)
+
+get_shop_list_by_dishes(['Запеченный картофель', 'Утка по-пекински', 'Фахитос'], 2)
+    
+
+
+
+
